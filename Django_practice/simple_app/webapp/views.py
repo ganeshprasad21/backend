a82ef29e063712e28561b661cbfa6f4454d2a133
Ganesh Prasad R <ganesh21111999@gmail.com>
2024-09-22 14:33:58 +0530

    added for loot in template

--- Django_practice/simple_app/webapp/views.py
+++ Django_practice/simple_app/webapp/views.py
@@ -11,7 +11,16 @@ def ganesh_prasad_r(request):
         # return HttpResponse(template.render())
         num = str(random.randint(3,303)) 
         print(num)
-        context = {'num':num, }
+        # myDict = {
+        #       'keyone':"val of key 1",
+        #       'keyanother':"val of key 1",
+        #       'keythree':"val of key 1",
+        #       'keyfour':"val of key 1",
+        #       'keyfive':"val of key 1",
+        #       'keysix':"val of key 1",
+        # }
+        myDict = ["item1","item 2","item3"]
+        context = {'num':num, 'myDict': myDict}
         template = get_template('w_.html')
         res = template.render(context,request)
         # print(HttpResponse(res))
