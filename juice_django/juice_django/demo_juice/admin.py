b4f3f7dd313f4ebfba1b259fd40cf019406f6f30
Ganesh Prasad R <ganesh21111999@gmail.com>
2024-09-24 18:34:03 +0530

    del

--- juice_django/juice_django/demo_juice/admin.py
+++
@@ -1,6 +0,0 @@
-from .models import Juice
-from django.contrib import admin
-# Register your models here.
-
-
-admin.site.register(Juice)
--- juice_django/juice_django/demo_juice/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('juices', views.juices),
-    path('get_juices', views.get_juices),
-
-    
-]
